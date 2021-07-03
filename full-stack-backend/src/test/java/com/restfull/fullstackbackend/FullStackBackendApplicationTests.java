dfb3222a54b3727020a11e77a7d5f7a3a81bb507
raif37 <raifperwez37@gmail.com>
2021-07-03 15:55:04 +0530

    1st commit

---
+++ full-stack-backend/src/test/java/com/restfull/fullstackbackend/FullStackBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restfull.fullstackbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullStackBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
